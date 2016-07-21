4c8645c07aab3782b8b4fff168063d22d8602221
JakubMrozek <jakub.mrozek@gmail.com>
2016-07-21 15:33:43 +0200

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+Server pro ukládání požadavků do PostgreSQL.
+
+# Spuštění
+
+```
+DB_URL=postgres://username:pass@server:5432/dbname?ssl=true node start
+```
+
+# Licence
+
+MIT
---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const pgp = require('pg-promise')()
+
+const PORT = process.env.PORT || 8000
+const DB_URL = process.env.DB_URL
+
+const db = pgp(DB_URL)
+
+const app = express()
+app.all('*', processRequst)
+app.listen(PORT)
+
+
+function processRequst(req, res, next) {
+  insertRow(req.headers, req.query, req.body)
+  .then(() => {
+    res.status(204)
+    res.end()
+  })
+  .catch(next)
+}
+
+function insertRow(headers, query, body) {
+  const sql = 'INSERT INTO requests (headers, query, body) VALUES ($1, $2, $3)'
+  const values = [headers, query, body].map(value => JSON.stringify(value))
+  return db.none(sql, values)
+}
